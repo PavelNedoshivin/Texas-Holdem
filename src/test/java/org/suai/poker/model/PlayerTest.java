2c20a5a68e011b511e4132ec7225d87a44fcac4e
ppned <ppnedoshivin98@mail.ru>
2018-12-23 20:24:01 +0300

    Small update

--- src/test/java/org/suai/poker/model/PlayerTest.java
+++ src/test/java/org/suai/poker/model/PlayerTest.java
@@ -6,28 +6,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    private Player test = new Player("Pavel", 100500, new Table(), false);
     @Test
     void payBlind() {
+        test.payBlind(500);
+        assertEquals(new Integer(500), test.getCurrentBet());
     }
 
     @Test
     void fold() {
+        test.fold();
+        assertEquals(PlayerStatus.PLAYER_FOLD, test.getStatus());
     }
 
     @Test
     void check() {
+        test.check();
+        assertEquals(PlayerStatus.PLAYER_CHECK, test.getStatus());
     }
 
     @Test
     void call() {
+        Table table = new Table();
+        table.setCurrentBet(500);
+        test.setTable(table);
+        test.payBlind(200);
+        test.call();
+        assertEquals(new Integer(500), test.getCurrentBet());
     }
 
     @Test
     void bet() {
+        test.bet(1000);
+        assertEquals(new Integer(1000), test.getCurrentBet());
     }
 
     @Test
     void raise() {
+        test.raise(1500);
+        assertEquals(PlayerStatus.PLAYER_RAISE, test.getStatus());
     }
 
     @Test
