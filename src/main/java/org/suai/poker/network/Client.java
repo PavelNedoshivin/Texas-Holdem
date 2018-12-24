508fdd9eb5f3db3049ed9fc1fb118958b112c60a
ppned <ppnedoshivin98@mail.ru>
2018-12-24 17:03:33 +0300

    Small update

--- src/main/java/org/suai/poker/network/Client.java
+++ src/main/java/org/suai/poker/network/Client.java
@@ -104,7 +104,7 @@ public class Client {
                 choseTable = true;
                 boolean first = true;
                 try {
-                    Thread.sleep(1000000000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
